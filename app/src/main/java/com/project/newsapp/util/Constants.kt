b6de4f9f27c72e61c4d6a1b1a1d9ea8f88942ba7
Chaima Bouhlel <chaima.bouhlel@insat.u-carthage.tn>
2024-01-21 16:04:02 +0100

    set retrofit

---
+++ app/src/main/java/com/project/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.project.newsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "bad136a9d599436e97a36e6034faf3c0"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
